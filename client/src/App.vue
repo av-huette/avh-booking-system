358c68964e56990b07115e63cf4346677f3acb12
ebert.l <info@leon-ebert.de>
2025-06-29 18:42:35 +0200

    Initialized Frontend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,30 @@
-<template>
-  <div id="app">
-    <NavBar></NavBar>
-    <router-view></router-view>
-    <Notification />
-    <FeedbackButton />
-  </div>
-</template>
-
-<script>
-import NavBar from "./components/NavBar.vue";
-import Notification from "./components/Notification.vue";
-import FeedbackButton from "./components/FeedbackButton.vue";
-
-export default {
-  name: "App",
-  components: {
-    NavBar,
-    Notification,
-    FeedbackButton,
-  },
-};
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
-<style>
-#app {
-  text-align: center;
-  color: #000000;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>

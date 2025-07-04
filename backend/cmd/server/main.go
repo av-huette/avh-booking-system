89eb6938167d0bd4d386afb749544c542e402f6c
maseiler <matthiasseiler@outlook.com>
2025-07-04 21:35:30 +0200

    Fix route

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
@@ -8,15 +9,21 @@ import (
 )
 
 func main() {
-	staticPath := os.Getenv("AVHBS_FRONTEND_PATH")
-	if staticPath == "" {
-		panic("Environment variable AVHBS_FRONTEND_PATH is not set. Make sure to set it to `<path to avh-booking-system>/frontend/dist`")
+	// load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file: " + err.Error())
 	}
 
-	mux := http.NewServeMux()
+	// set up file server
+	staticPath := os.Getenv("AVHBS_FRONTEND_PATH")
 	fileServer := http.FileServer(http.Dir(staticPath))
-	mux.Handle("GET /{$}", fileServer)
 
+	// set up routes
+	mux := http.NewServeMux()
+	mux.Handle("GET /", fileServer)
+
+	// set up web server
 	server := &http.Server{
 		Addr:           ":8081",
 		Handler:        mux,
@@ -24,8 +31,7 @@ func main() {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-
-	log.Print("Starting backend on :8081")
-	err := server.ListenAndServe()
+	log.Print("Starting server on :8081")
+	err = server.ListenAndServe()
 	log.Fatal(err)
 }
